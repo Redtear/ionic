6e80da33e655b1e6e940ceb99c2923f080ddbb16
Brandy Carney <brandyscarney@gmail.com>
2016-06-22 11:45:07 -0400

    docs(item): update docs to include the list header and item divider

--- src/components/item/item.ts
+++ src/components/item/item.ts
@@ -17,16 +17,34 @@ import { Label } from '../label/label';
  *
  *
  * ## Common Usage
- * An item can be written as an `<ion-item>` element or it can be added to any element by adding
- * `ion-item` as an attribute.
+ * There are a few elements that are considered items, but not all of them are styled to look the same.
+ * The basic item can be written as an `<ion-item>` element or it can be added to any element by adding
+ * `ion-item` as an attribute. List headers and item dividers, although styled differently, are also items
+ * and can be written as `<ion-list-header>` and `<ion-item-divider>`, respectively.
  *
  * ### As an Element
- * An item should be written as a `<ion-item>` element when it is not clickable.
+ * A basic item should be written as a `<ion-item>` element when it is not clickable.
  *
  * ```html
  * <ion-item>
  *   Item
  * </ion-item>
+ * ```
+ *
+ * A list header should be written as `<ion-list-header>`.
+ *
+ * ```html
+ * <ion-list-header>
+ *   List Header
+ * </ion-list-header>
+ * ```
+ *
+ * An item divider should be written as `<ion-item-divider>`.
+ *
+ * ```html
+ * <ion-item-divider>
+ *   Item Divider
+ * </ion-item-divider>
  * ```
  *
  * ### As an Attribute
@@ -43,6 +61,9 @@ import { Label } from '../label/label';
  *   Anchor Item
  * </a>
  * ```
+ *
+ * Note: do not add `ion-item` as an attribute to an `<ion-list-header>` or `<ion-item-divider>` element
+ * as they are already items and their styling will be changed to look like a basic item.
  *
  * ## Detail Arrows
  * By default, `<button>` and `<a>` elements with the `ion-item` attribute will display a right arrow icon
@@ -107,6 +128,10 @@ import { Label } from '../label/label';
  * ```html
  * <ion-list>
  *
+ *   <ion-list-header>
+ *     Header
+ *   </ion-list-header>
+ *
  *   <ion-item>
  *     Item
  *   </ion-item>
@@ -118,6 +143,10 @@ import { Label } from '../label/label';
  *   <button ion-item (click)="buttonClick()">
  *     Button Item
  *   </button>
+ *
+ *   <ion-item-divider>
+ *     Item Divider
+ *   </ion-item-divider>
  *
  *   <button ion-item detail-none (click)="buttonClick()">
  *     Button Item with no Detail Arrow
@@ -144,6 +173,13 @@ import { Label } from '../label/label';
  *
  * ```html
  * <ion-list>
+ *
+ *   <!-- List header with buttons on each side -->
+ *   <ion-list-header>
+ *     <button item-left (click)="buttonClick()">Button</button>
+ *     List Header
+ *     <button outline item-right (click)="buttonClick()">Outline</button>
+ *   </ion-list-header>
  *
  *   <!-- Loops through and creates multiple items -->
  *   <ion-item *ngFor="let item of items">
@@ -172,6 +208,12 @@ import { Label } from '../label/label';
  *     Item
  *     <button outline item-right (click)="buttonClick()">Outline</button>
  *   </ion-item>
+ *
+ *   <!-- Item divider with a right button -->
+ *   <ion-item-divider>
+ *     Item Divider
+ *     <button item-right>Button</button>
+ *   </ion-item-divider>
  *
  *   <!-- Disabled button item with left and right buttons -->
  *   <button ion-item disabled>
@@ -369,7 +411,9 @@ export class Item {
   }
 }
 
-
+/**
+ * @private
+ */
 @Directive({
   selector: 'ion-item,[ion-item]',
   host: {
